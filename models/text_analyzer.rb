9e0f9dca450ee7200f4104bba7a9fe538eab8e09
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-19 02:28:09 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,48 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+
+  attr_accessor :text
+
+  def initialize(text)
+    @text = text
+  end
+
+  def count_of_words
+    words = @text.split(" ")
+    words.count
+  end
+
+  def count_of_vowels
+    @text.scan(/[aeoui]/).count
+  end
+
+  def count_of_consonants
+    @text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+  end
+
+  def most_used_letter
+    s1 = @text.downcase.gsub(/[^a-z]/, '') #gets rid of spaces
+    arr = s1.split('')
+    arr1 = arr.uniq
+    arr2 = {}
+    arr1.map do |c| 
+     arr2[c] =  arr.count(c)
+    end
+    biggest = { 
+      :most_used_letter  => arr2.keys.first,
+      :letter_count => arr2.values.first 
+    }
+    arr2.each do |key, value|
+      if value > biggest.values[1]
+        biggest = {
+          :most_used_letter => "",
+          :letter_count => ""
+        }
+        biggest[:most_used_letter] = key
+        biggest[:letter_count] = value
+      end
+    end
+    biggest
+  end
+
+end
