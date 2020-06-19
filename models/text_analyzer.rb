f12f804ae49cec577bafa5e5094087ca00bf5ddf
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-19 02:38:46 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -20,29 +20,20 @@ class TextAnalyzer
     @text.downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).count
   end
 
-  def most_used_letter
-    s1 = @text.downcase.gsub(/[^a-z]/, '') #gets rid of spaces
-    arr = s1.split('')
-    arr1 = arr.uniq
-    arr2 = {}
-    arr1.map do |c| 
-     arr2[c] =  arr.count(c)
-    end
-    biggest = { 
-      :most_used_letter  => arr2.keys.first,
-      :letter_count => arr2.values.first 
-    }
-    arr2.each do |key, value|
-      if value > biggest.values[1]
-        biggest = {
-          :most_used_letter => "",
-          :letter_count => ""
-        }
-        biggest[:most_used_letter] = key
-        biggest[:letter_count] = value
+def most_used_letter
+    all_letters_in_string = @text.downcase.gsub(/[^a-z]/, '').split('') #gets rid of spaces and turns it into an array
+    letters_to_compare = all_letters_in_string.uniq
+    most_used_letter = ""
+    letter_count = 0
+
+    letters_to_compare.map do |letter|
+      letter_repetitions = all_letters_in_string.count(letter)
+      if letter_repetitions > letter_count
+        letter_count = letter_repetitions
+        most_used_letter = letter
       end
     end
-    biggest
+    biggest = [most_used_letter, letter_count]
   end
 
 end
